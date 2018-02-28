ad11a52af252a5ec5d27adaadf49a287bad2ff1b
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2018-02-28 14:41:01 -0800

    Added tests and questions

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # Precourse-Assessment
 
+## Prerequisites
+
+In order to complete this assessment you will need to have a basic understanding of the following concepts:
+```
+JavaScript
+  variables
+  if statements
+  for loops
+  functions
+  arrays
+  objects
+  math operators
+  equality and logical operators
+Git/GitHub
+  fork
+  clone
+  add
+  commit
+  push
+  pull request
+```
+## Instructions
+
+Instructions for taking this assessment:
+
+You will need to download Node in order to run the automated tests.  You can download Node (here)['www.google.com'].
+
+Fork this repo to your GitHub account.
+
+Clone the forked copy to your local computer:  `git clone <fork url>`
+
 fork, clone, npm install
 
 complese the assessment in `assessment.js`.
@@ -11,4 +42,7 @@ When you get all of the tests to pass:
 add, commit, and push
 
 submit a pull request on GitHub
+
+
+## Resources
 
--- assessment.test.js
+++ assessment.test.js
@@ -1,6 +1,8 @@
 const {
   helloWorld,
   lambdaSchool,
+  longestString,
+  computeUserAverageAge,
 } = require('./assessment'); 
 
 describe('Lambda School Precourse Assessment', () => {
@@ -12,6 +14,7 @@ describe('Lambda School Precourse Assessment', () => {
       expect(helloWorld()).toBe('Hello World!');
     });
   }); 
+
   describe('lambdaSchool', () => {
     it('should return \'Lambda\' for a number divisible by 3', () => {
       expect(lambdaSchool(12)).toBe('Lambda');
@@ -29,5 +32,44 @@ describe('Lambda School Precourse Assessment', () => {
       expect(lambdaSchool(180)).toBe('Lambda School');
     });
   }); 
+
+  describe('longestString', () => {
+    it('should return the longest string in the array', () => {
+      expect(longestString(['array', 'object', 'function'])).toBe('function');
+      expect(longestString(['C++', 'JavaScript', 'Python'])).toBe('JavaScript');
+    });
+    it('should return the first longest string if there is a tie', () => {
+      expect(longestString(['C++', 'CSS', 'JWT'])).toBe('C++');
+    });
+  }); 
+
+  describe('computeUserAverageAge', () => {
+    const authors = [{
+      name: 'Frank Herbert',
+      age: 65,
+    }, {
+      name: 'Douglas Adams',
+      age: 49,
+    }, {
+      name: 'Isaac Asimov',
+      age: 72,
+    }];
+    const computerScientists = [{
+      name: 'Brendan Eich',
+      age: 56,
+    }, {
+      name: 'Linus Torvalds',
+      age: 48,
+    }, {
+      name: 'Margaret Hamilton',
+      age: 81,
+    }];
+    it('should return the average age of the users', () => {
+      expect(computeUserAverageAge(authors)).toBe(62);
+    });
+    it('should round the average before returning it', () => {
+      expect(computeUserAverageAge(computerScientists)).toBe(62);
+    });
+  });
 });
 
